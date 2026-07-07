19268c2b4051eec09a78058e7561d444e844433b
akshatsonic <akshatsoni1529@gmail.com>
2026-07-07 10:41:15 +0530

    add LaTeX math rendering support with katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 import tailwind from '@astrojs/tailwind'
 
@@ -9,6 +11,8 @@ export default defineConfig({
     site: 'https://akshatsonic.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
         shikiConfig: {
           // Choose from Shiki's built-in themes (or add your own)
           // https://shiki.style/themes
